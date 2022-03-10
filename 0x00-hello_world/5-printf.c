2715b8e9b7da710d1921eb032a9b9344433c63b6
Owinnie <tillawinnie@gmail.com>
2022-03-10 13:48:35 -0800

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * main - my main function
+  * takes no parameters
+  * prints something
+  * returns a value
+  * Return: 0
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
