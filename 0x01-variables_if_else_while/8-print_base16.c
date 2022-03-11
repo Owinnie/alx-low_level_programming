05bafdd2d3fa736f6b508291e75c4ff54f57a79c
Owinnie <tillawinnie@gmail.com>
2022-03-11 06:00:40 -0800

    8. Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,11 +7,19 @@ int main(void)
 {
 	char num;
 
-	num = '0x0';
-	while (num <= '0xf')
+	num = '0';
+	while (num <= '9')
 	{
 		putchar(num);
 		num += 1;
+	}
+
+	char letter = 'a';
+
+	while (letter <= 'f')
+	{
+		putchar(letter);
+		letter += 1;
 	}
 	putchar('\n');
 	return (0);
