161b40c154935c6fae58f7731df813cabb7fc0fe
Owinnie <tillawinnie@gmail.com>
2022-03-15 19:40:35 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,10 +8,29 @@
   */
 void print_to_98(int n)
 {
-	while (n <= 98)
+	if (n > 98)
 	{
-		printf("%d, ", n);
-		n++;
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+		        n--;
+		}
 	}
-	putchar('\n');
+	else
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	printf("\n");
 }
