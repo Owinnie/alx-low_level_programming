43580285997f8e0741767fca6bbc5e372bf6b115
Owinnie <tillawinnie@gmail.com>
2022-03-16 09:42:35 -0700

    0. Multiple mains

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,13 +1,8 @@
-#include "main.h"
-/**
-  * main - function main
-  * Return: 0
-  */
-int main(void)
-{
-	int i;
+#ifndef MAIN_H
+#define MAIN_H
 
-	i = 0;
-	positive_or_negative(i);
-	return (0);
-}
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
