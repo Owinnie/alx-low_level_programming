095bd330502955bad5ab48ad315958cc9bea911d
Owinnie <tillawinnie@gmail.com>
2022-03-16 08:57:13 -0700

    0. Multiple mains

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-
 void positive_or_negative(int i);
-
-#endif /* MAIN_H */
 int _putchar(int c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
