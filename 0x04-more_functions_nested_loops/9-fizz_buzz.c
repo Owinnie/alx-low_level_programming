651bd1f800dc4bcc6c96fcc073c9cda80b50597a
Owinnie <tillawinnie@gmail.com>
2022-03-17 16:02:30 -0700

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,13 +11,28 @@ int main(void)
 	for (nums = 1; nums <= 100; nums++)
 	{
 		if (nums % 3 == 0 && nums % 5 == 0)
+		{
 			printf("%s ", "FizzBuzz");
+		}
 		else if (nums % 3 == 0)
+		{
 			printf("%s ", "Fizz");
-		else if (nums % 5 == 0)
-			printf("%s ", "Buzz");
-		else
+		}
+		else if (nums % 3 != 0 && nums % 5 != 0)
+		{
 			printf("%d ", nums);
+		}
+		else
+		{
+			if (nums % 5 == 0 && nums == 100)
+			{
+				printf("%s", "Buzz");
+			}
+			else
+			{
+				printf("%s ", "Buzz");
+			}
+		}
 	}
 	printf("\n");
 	return (0);
