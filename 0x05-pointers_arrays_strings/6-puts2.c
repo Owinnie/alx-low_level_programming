e3dae8f274488b0df165768db78f50bb068dc03d
Owinnie <tillawinnie@gmail.com>
2022-03-21 13:24:08 -0700

    6.

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,13 @@ void puts2(char *str)
 	int index;
 
 	index = 0;
-	while (*str != '\0')
+	while (str[index] != '\0')
 	{
 		if (index % 2 == 0)
 		{
 			_putchar(str[index]);
 		}
+		index++;
 	}
 	_putchar('\n');
 }
