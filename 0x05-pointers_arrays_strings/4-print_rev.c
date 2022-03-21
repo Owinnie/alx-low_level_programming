098d31b4cdd7b28f437008e1977379222c9404d9
Owinnie <tillawinnie@gmail.com>
2022-03-21 11:01:14 -0700

    4. I can only go one way. I've not got a reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,10 +5,18 @@
   */
 void print_rev(char *s)
 {
+	int index = 0;
+
 	while (*s != '\0')
+	{
+		index++;
+		s++;
+	}
+	while (index > 0)
 	{
 		s--;
 		_putchar(*s);
+		index--;
 	}
 	_putchar('\n');
 }
