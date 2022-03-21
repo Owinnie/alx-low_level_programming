301612e41a00691dd13b13e9824ac9dc8073e420
Owinnie <tillawinnie@gmail.com>
2022-03-21 10:27:32 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * _strlen - find string length
+  * @s: parameter
+  * Return: int
+  */
+int _strlen(char *s)
+{
+	int count;
+	char index;
+
+	index = s[0];
+	while (index != '\0')
+	{
+		count++;
+		index++;
+	}
+	return (count);
+}
