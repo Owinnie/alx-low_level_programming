085d0c45ddbfef1cff767c5ca734b541f8d4bcf7
Owinnie <tillawinnie@gmail.com>
2022-03-21 13:39:17 -0700

    5.

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,17 +5,20 @@
   */
 void rev_string(char *s)
 {
+	int index = 0;
 	char *w = s;
 
 	while (*w != '\0')
 	{
+		index++;
 		_putchar(*w);
 		w++;
 	}
 	_putchar('\n');
-	while (w--)
+	while (w[index] != '\0')
 	{
-		_putchar(*w);
+		_putchar(w[index]);
+		index--;
 	}
 	_putchar('\n');
 }
