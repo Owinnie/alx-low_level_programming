a81675b52e84766d5318511497f8ddb9c2a5bab6
Owinnie <tillawinnie@gmail.com>
2022-03-28 10:32:01 -0700

    3. strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,5 +9,12 @@
   */
 unsigned int _strspn(char *s, char *accept)
 {
-	return (0);
+	int index;
+	int jndex;
+
+	for (jndex = 0; accept[jndex] != '\0'; jndex++)
+		;
+	for (index = 0; index <= jndex && s[index] != '\0'; index++)
+		;
+	return (index);
 }
