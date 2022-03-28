d58862f5af3a3aa379c4f518c7e7368137577a5b
Owinnie <tillawinnie@gmail.com>
2022-03-28 12:47:53 -0700

    6. Chess is mental torture

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,4 +6,14 @@
   */
 void print_chessboard(char (*a)[8])
 {
+	int index, jndex;
+
+	for (index = 0; index < 8; index++)
+	{
+		for (jndex = 0; jndex < 8; jndex++)
+		{
+			_putchar(a[index][jndex]);
+		}
+		_putchar('\n');
+	}
 }
