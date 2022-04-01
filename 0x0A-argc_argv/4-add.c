fde3fc705638d5b73de8b22f52c247f099364d76
Owinnie <tillawinnie@gmail.com>
2022-04-01 15:50:57 -0700

    4. To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - argc and argv
+  * @argc: para1
+  * @argv: para2
+  *
+  * Return: int
+  */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (!(*argv[i] >= '0') || !(*argv[i] <= '9'))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+				sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
