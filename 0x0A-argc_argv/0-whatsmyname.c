e548526079430a64c3393465fc5dfbf770b38567
Owinnie <tillawinnie@gmail.com>
2022-04-01 14:33:50 -0700

    0.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - argc and argv
+  * @argc: para1
+  * @argv: para2
+  *
+  * Return: int
+  */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s\n", argv[argc]);
+	return (0);
+}
