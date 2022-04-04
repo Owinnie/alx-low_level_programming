fb60a703de12f79cc3afcfb8b2b298499747afdc
Owinnie <tillawinnie@gmail.com>
2022-04-04 14:57:32 -0700

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,6 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+  * _strlen - find len of a string
+  * @s: string
+  * Return: int
+  */
+int _strlen(char *s)
+{
+	int size = 0;
+
+	for (; s[size] != '\0'; size++)
+		;
+	return (size);
+}
+/**
   * str_concat - concat 2 strings
   * @s1: para1
   * @s2: para2
@@ -10,20 +23,23 @@
   */
 char *str_concat(char *s1, char *s2)
 {
-	int i, j;
+	int i;
 	char *s;
 
 	if (s1 == NULL)
 		s1 = '\0';
 	if (s2 == NULL)
 		s2 = '\0';
-	s = malloc(sizeof(*s1) + sizeof(*s2));
+	s = malloc((_strlen(s1) + _strlen(s2)) * (2 * sizeof(char)));
 	if (s == 0)
 		return (NULL);
-	for (i = 0; s1[i] != '\0'; i++)
-		s[i] = s1[i];
-	for (j = 0; j < i && s2[j] != '\0'; i++, j++)
-		s[i] = s2[j];
+	for (i = 0; i < _strlen(s1) + _strlen(s2); i++)
+	{
+		if (i < _strlen(s1))
+			s[i] = s1[i];
+		else
+			s[i] = s2[i - _strlen(s1)];
+	}
 	s[i] = '\0';
 	return (s);
 }
