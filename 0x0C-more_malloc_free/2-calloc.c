c5966502f322e412503e962a6b414ed083f47c63
Owinnie <tillawinnie@gmail.com>
2022-04-07 10:49:25 -0700

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,4 +9,15 @@
   */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	unsigned int i;
+	int *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * sizeof(size));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < sizeof(ptr); i++)
+		ptr[i] = 0;
+	return (ptr);
 }
