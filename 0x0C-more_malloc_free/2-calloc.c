25a13864b17131d1a420fe440effb8419f1f108c
Owinnie <tillawinnie@gmail.com>
2022-04-07 11:09:32 -0700

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,6 +2,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+  * _memset - fill with a constant byte
+  * @t: pointer
+  * @b: constant
+  * @n: max bytes
+  * Return: t
+  */
+char *_memset(char *t, char b, unsigned int n)
+{
+	char *str = t;
+
+	while (n--)
+		*t++ = b;
+	return (str);
+}
+/**
   * _calloc - array malloc allocate
   * @nmemb: para1
   * @size: para2
