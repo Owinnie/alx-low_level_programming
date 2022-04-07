e4003ae538f6b1801fe4237423f91b7ccd0b3e34
Owinnie <tillawinnie@gmail.com>
2022-04-07 10:36:33 -0700

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,13 +10,29 @@
   */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int index;
+	unsigned int i, j, l1, l2;
 	char *ptr;
 
-	ptr = realloc(s1, 2 * sizeof(s1));
-	for (index = 0; index < n; index++)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (l1 = 0; s1[l1] != '\0'; l1++)
+		;
+	for (l2 = 0; s2[l2] != '\0'; l2++)
+		;
+	ptr = malloc(l1 + n + 1);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
 	{
-		ptr[sizeof(s1) + 1] = s2[index];
+		ptr[i] = s1[i];
 	}
+	for (j = 0; j < n; j++)
+	{
+		ptr[i] = s2[j];
+		i++;
+	}
+	ptr[i] = '\0';
 	return (ptr);
 }
