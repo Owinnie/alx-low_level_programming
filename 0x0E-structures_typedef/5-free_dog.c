1a284a68335c6429f1031975cc844446f1a6d048
Owinnie <tillawinnie@gmail.com>
2022-04-11 12:29:54 -0700

    5. How many legs

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -6,10 +6,9 @@
   */
 void free_dog(dog_t *d)
 {
-	if (d != NULL)
-	{
-		free(d);
-		free(d->name);
-		free(d->owner);
-	}
+	if (d == NULL)
+		return;
+	free(d);
+	free(d->name);
+	free(d->owner);
 }
