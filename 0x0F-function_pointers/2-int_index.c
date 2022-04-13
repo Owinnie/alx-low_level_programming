1a761bce9d162bb4320b062f575b8de7aa756f72
Owinnie <tillawinnie@gmail.com>
2022-04-13 11:11:19 -0700

    2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+  * int_index - search for int
+  * @array: para1
+  * @size: para2
+  * @cmp: para3
+  *
+  * Return: int
+  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+}
