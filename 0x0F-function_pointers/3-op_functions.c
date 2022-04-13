b5e3a3b90dbec4375c4df939bf189d3125771a55
Owinnie <tillawinnie@gmail.com>
2022-04-13 13:07:08 -0700

    3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,2 +1,56 @@
 #include "3-calc.h"
-
+/**
+  * op_add - addition
+  * @a: para1
+  * @b: para2
+  *
+  * Return: int
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+  * op_sub - subtraction
+  * @a: para1
+  * @b: para2
+  *
+  * Return: int
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+  * op_mul - multiplication
+  * @a: para1
+  * @b: para2
+  *
+  * Return: int
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+  * op_div - division
+  * @a: para1
+  * @b: para2
+  *
+  * Return: int
+  */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+  * op_mod - modulus
+  * @a: para1
+  * @b: para2
+  *
+  * Return: int
+  */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
