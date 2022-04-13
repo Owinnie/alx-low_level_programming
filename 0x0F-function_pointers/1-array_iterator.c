0bdd6b660e0784ab9334b40161c6ed2062fd5a3a
Owinnie <tillawinnie@gmail.com>
2022-04-13 11:08:08 -0700

    0

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+  * array_iterator - execute function as para
+  * @array: para1
+  * @size: para2
+  * @action: para3
+  */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+}
