55f6927542909183a90c37e241c99485c14a3fe1
Owinnie <tillawinnie@gmail.com>
2022-04-14 09:31:34 -0700

    2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * print_strings - print strings
+  * @separator: para1
+  * @n: para2
+  */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+}
