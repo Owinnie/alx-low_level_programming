13da7383f968ae6f48bf03aeb4544a400c7371e7
Owinnie <tillawinnie@gmail.com>
2022-04-14 09:35:09 -0700

    3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,10 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * print_all - print anything
+  * @format: para1
+  */
+void print_all(const char * const format, ...)
+{
+}
