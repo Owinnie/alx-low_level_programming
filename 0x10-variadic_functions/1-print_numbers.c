3c3dee8429470a11beae30bca3158d6d49bc2fed
Owinnie <tillawinnie@gmail.com>
2022-04-14 09:18:51 -0700

    1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * print_numbers - print nums
+  * @separator: const char
+  * @n: const int
+  */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+}
