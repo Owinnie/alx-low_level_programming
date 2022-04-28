a2242e3395e8774076846bcb34012e438ea90cc5
Owinnie <tillawinnie@gmail.com>
2022-04-28 14:00:53 -0700

    2. 10

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  * get_bit - value of bit at given indx
+  * @n: num to look thro
+  * @index: indx used
+  *
+  * Return: value of the bit at index or -1
+  */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int shtd, and;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	shtd = 1 << index;
+	and = n & shtd;
+	if (and == shtd)
+		return (-1);
+	return (0);
+}
