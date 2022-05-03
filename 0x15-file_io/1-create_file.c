5dbbc573384511aa3663f82d5d82c32bf5a72fdd
Owinnie <tillawinnie@gmail.com>
2022-05-03 16:52:51 -0700

    1.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,11 +16,9 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
 	if (fd == -1)
 		return (-1);
-	for (i = 0; text_content[i] != '\0'; i++)
-		;
 	if (text_content != NULL)
 	{
-		write_output = write(fd, text_content, i);
+		write_output = write(fd, text_content, len(text_content));
 		if (write_output == -1)
 		{
 			close(fd);
@@ -29,4 +27,20 @@ int create_file(const char *filename, char *text_content)
 	}
 	close(fd);
 	return (1);
+}
+/**
+  * len - length of a string
+  * @s: input char
+  * Return: length of a string
+  */
+
+int len(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
 }
