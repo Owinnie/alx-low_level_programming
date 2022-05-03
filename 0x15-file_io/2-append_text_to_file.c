704c9055d0d5bd46ab560dbc50f9b90f641998b8
Owinnie <tillawinnie@gmail.com>
2022-05-03 13:26:59 -0700

    3.

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * append_text_to_file - append txt at EOF
+  * @filename: path
+  * @text_content: null-terminated str
+  *
+  * Return: 1 on success and -1 on failure
+  */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	return (0);
+}
