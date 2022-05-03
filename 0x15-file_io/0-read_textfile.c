f39efac5e6fff2cb1b7c724390824c9211a8020d
Owinnie <tillawinnie@gmail.com>
2022-05-03 16:25:23 -0700

    0.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,5 +8,36 @@
   */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	return (letters);
+	int fd;
+	char *buf;
+	ssize_t read_output;
+
+	if (filename == NULL)
+		return (0);
+	buf = malloc(sizeof(char) * letters + 1);
+	if (buf == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	read_output = read(fd, buf, sizeof(char) * letters);
+	if (read_output == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	read_output = write(STDOUT_FILENO, buf, read_output);
+	if (read_output == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	free(buf);
+	close(fd);
+	return (read_output);
 }
