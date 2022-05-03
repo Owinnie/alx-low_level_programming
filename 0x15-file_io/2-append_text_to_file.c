6c9e740f040b83aa35da5d738a39adebbf9ad484
Owinnie <tillawinnie@gmail.com>
2022-05-03 16:07:23 -0700

    2.

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,5 +8,25 @@
   */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	return (0);
+	int i, fd;
+	ssize_t write_output;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
+	if (text_content != NULL)
+	{
+		write_output = write(fd, text_content, i);
+		if (write_output == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
 }
