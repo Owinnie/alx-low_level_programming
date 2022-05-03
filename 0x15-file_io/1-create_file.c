7f3500cf509be1bdd9a60147ce2718fb94542311
Owinnie <tillawinnie@gmail.com>
2022-05-03 13:23:24 -0700

    1.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * create_file - create a file
+  * @filename: path
+  * @text_content: null-terminated str
+  *
+  * Return: 1 on success, -1 on failure
+  */
+int create_file(const char *filename, char *text_content)
+{
+	return (0);
+}
