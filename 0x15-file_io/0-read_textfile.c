8666313207e2ac8c0817981f56634e5c2f4135be
Owinnie <tillawinnie@gmail.com>
2022-05-03 13:19:44 -0700

    0.

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * read_textfile - read and print
+  * @filename: file path
+  * @letters: no. to read and print
+  *
+  * Return: type ssize_t
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	return (letters);
+}
