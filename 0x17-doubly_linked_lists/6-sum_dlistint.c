c28a5ea7262c3b918b844aa479445e5e9361ec8b
Owinnie <tillawinnie@gmail.com>
2022-06-09 13:23:35 -0700

    6.

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+  * sum_dlistint(dlistint_t - sum total of data
+  * @head: list
+  * Return: sum of all the data (n), or 0
+  */
+int sum_dlistint(dlistint_t *head)
+{
+	int total = 0;
+
+	while (head != NULL)
+	{
+		total += head->n;
+		head = head->next;
+	}
+	return (total);
+}
