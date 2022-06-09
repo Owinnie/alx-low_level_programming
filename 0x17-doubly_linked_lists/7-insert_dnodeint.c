97bd475d80a7425a1f5ed27582b42eb8941f93ac
Owinnie <tillawinnie@gmail.com>
2022-06-09 13:33:44 -0700

    7.

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+  * insert_dnodeint_at_index - insert node at postn
+  * @h: ptr to ptr to head
+  * @idx: where to be inserted
+  * @n: data
+  * Return: address of the new node, or NULL
+  */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+}
