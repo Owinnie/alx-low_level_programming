7e33dc25bba0cbbb6aa53890aa09eccad964b0c4
Owinnie <tillawinnie@gmail.com>
2022-06-09 13:41:56 -0700

    8.

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+  * delete_dnodeint_at_index - del node
+  * @head: ptr to ptr to first node
+  * @index: where to insert
+  * Return: 1, or -1
+  */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+}
