cd59a555c77715f2bf0c92c7385d98fbe3b6953b
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-06-30 14:29:20 -0700

    3.

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,41 +1,44 @@
 #include "hash_tables.h"
-/**
- * make_hnode - creates hash node
- * @key: key
- * @value: value
- *
- * Return: new node, or NULL
- */
-hash_node_t *make_hnode(const char *key, const char *value)
-{
-	hash_node_t *nd;
 
-	nd = malloc(sizeof(hash_node_t));
-	if (nd == NULL)
-		return (NULL);
-	nd->key = strdup(key);
-	if (nd->key == NULL)
-	{
-		free(nd);
-		return (NULL);
-	}
-	nd->value = strdup(value);
-	if (nd->value == NULL)
-	{
-		free(nd->key);
-		free(nd);
-		return (NULL);
-	}
-	nd->next = NULL;
-	return (nd);
-}
 /**
- * hash_table_set - set key
- * @ht: hash table
- * @key: key
- * @value: value
+ * make_hash_node - creates a new hash node
+ * @key: key for the node
+ * @value: for the node
  *
- * Return: 1 or 0
+ * Return: the new node, or NULL on failure
+ */
+hash_node_t *make_hash_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (NULL);
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
+	node->next = NULL;
+	return (node);
+}
+
+
+/**
+ * hash_table_set - sets a key to a value in the hash table
+ * @ht: hash table to add elemt to
+ * @key: key for the data
+ * @value: data to store
+ *
+ * Return: 1 if successful, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
