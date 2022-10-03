3781bba6e21553f6b955709accc8a490ff5e3849
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-10-03 17:43:48 +0300

    header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,6 +7,7 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t i, size_t j);
 int jump_search(int *array, size_t size, int value);
+size_t minimum_value(size_t a, size_t b);
 int interpolation_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 
