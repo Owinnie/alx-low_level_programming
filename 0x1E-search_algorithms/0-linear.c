4c81c3c187a258ea210d6be8f2281cde77f0db2c
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-10-03 16:16:19 +0300

    Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,7 +11,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
-	for (; i < size; i++)
+	for (; (i < size) && (array); i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
