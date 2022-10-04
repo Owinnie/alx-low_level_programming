4c11d9565a9e975cca7b0fff5946fd65b21998af
Owinnie <tillawinnie@gmail.com>
2022-10-04 05:26:47 -0700

    header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,7 +10,8 @@ int jump_search(int *array, size_t size, int value);
 size_t minimum_value(size_t a, size_t b);
 int interpolation_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
-int advanced_binary_recursive(int *array, size_t left, size_t right, int value);
+int advanced_binary_recursive(int *array, size_t left,
+		size_t right, int value);
 
 /**
   * struct listint_s - singly linked list
